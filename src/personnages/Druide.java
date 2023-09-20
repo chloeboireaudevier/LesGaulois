5ebdb02bac748d8b7627e509960595b0d09f8b40
Chloe Boireau-devier <brc4847a@ens.fsi>
2023-09-20 15:30:02 +0200

    TP1 -> exercice 4

---
+++ src/personnages/Druide.java
@@ -0,0 +1,49 @@
+package personnages;
+
+import java.util.Random;
+
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	private int forcePotion = 1;
+	
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide "+nom+" et ma potion peut aller d'une force "+effetPotionMin+" à "+effetPotionMax+".");
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte +"»");
+		
+	}
+	
+	public int preparerPotion() {
+		Random random = new Random();
+		int force = random.nextInt(effetPotionMax);
+		if (force > effetPotionMin) {
+			parler("J'ai préparé une super potion de force " + force);
+		} else {
+			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force "+effetPotionMin);
+		}
+		return force;
+	}
+	
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+	
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+		panoramix.preparerPotion();
+		panoramix.preparerPotion();
+	}
+}
